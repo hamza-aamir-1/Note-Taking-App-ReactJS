d0ec659d25ddaf5783fbea8672c683e0b17ad6eb
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-03-04 23:40:22 +0500

    Styling Note Taking App

---
+++ src/components/AddNote/AddNote.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './AddNote.css'
+
+const AddNote = () => {
+  return (
+    <div>
+        <div className='box'>
+            <textarea name="" id="" rows="10" className='input' placeholder='Add Note Here....'></textarea>
+        </div>
+    </div>
+  )
+}
+
+export default AddNote
