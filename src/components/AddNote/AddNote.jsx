9d32733ccfa300b8f72d38dcddc7074fabc39443
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-03-14 23:16:59 +0500

    Adding & Showing Notes Done

--- src/components/AddNote/AddNote.jsx
+++ src/components/AddNote/AddNote.jsx
@@ -5,14 +5,12 @@ const AddNote = (props) => {
 
 return (
     <div>
-        <div>
-          <form onSubmit={props.submitNote}>
-              <textarea onChange={props.changeNote} value={props.noteValue} placeholder='Add Note Here....'></textarea>
-              <div>
-                  <button type='submit'>ADD</button>
-              </div>
-          </form>
+      <form className='box' onSubmit={props.submitNote}>
+        <textarea className='input' rows={5} onChange={props.changeNote} value={props.noteValue} placeholder='Add Note Here....'></textarea>
+        <div className='btn-box'>
+          <button type='submit'>ADD</button>
         </div>
+      </form>
     </div>
   )
 }
